99195d6422bdb98da26c8ee4a818048e1cbadfb8
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-10-23 13:07:16 +0300

    Task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,17 @@
+import profile from './json_files/user.json'
+import {Profile}from './Profile/profile'
+
 export const App = () => {
-  return ;
+  return <div>
+    <Profile 
+    username={profile.username}
+     tag={profile.tag} 
+     location={profile.location} 
+     avatar={profile.avatar} 
+     followers={profile.stats.followers}
+      views={profile.stats.views} 
+      likes={profile.stats.likes} />
+  </div>;
 };
+
+
