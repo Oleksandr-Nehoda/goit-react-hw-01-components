ebc03547e1533c9e232f2d64c39c3fe3f1f7cb19
Oleksandr-Nehoda <iamsoul@ukr.net>
2022-10-24 13:14:16 +0300

    task4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import profile from './json_files/user.json';
 import stats from './json_files/data.json';
 import friends from './json_files/friends.json';
+import transactions from './json_files/transactions.json'
 import { Profile } from './Profile/profile'
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import {TransactionHistory} from './TransactionHistory/TransactionHistory'
 
 export const App = () => {
   return (
@@ -19,6 +21,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={stats} />
       <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions}/>
     </div>
   );
 };
